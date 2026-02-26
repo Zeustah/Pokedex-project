5454586b6ddecf5985356be9dce9c8762d10621c
Zeustah <ar8358@gmail.com>
2026-02-26 11:27:28 -0500

    (3) added a REPL and command line interaction

--- src/repl.ts
+++ src/repl.ts
@@ -2,3 +2,22 @@ export function cleanInput(input: string): string[] {
   const lowered = input.toLowerCase();
   return lowered.trim().split(/\s+/);
 }
+
+import { createInterface } from "node:readline";
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+  rl.prompt();
+  rl.on("line", (callback: string) => {
+    if (!callback) {
+      rl.prompt();
+    } else {
+      const cleanCallback = cleanInput(callback);
+      console.log(`Your command was: ${cleanCallback[0]}`);
+      rl.prompt();
+    }
+  });
+}
